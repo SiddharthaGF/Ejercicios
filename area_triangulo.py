2bd44359f25b1af27459230bd61132b165911689
= <pablo.guerra@espoch.edu.ec>
2022-09-29 09:41:55 -0500

    first commit

---
+++ area_triangulo.py
@@ -0,0 +1,71 @@
+from asyncio.windows_events import NULL
+import math
+from re import M
+
+
+class Excepcion(ValueError):
+    def __init__(self, message, * args):
+        super(Excepcion, self).__init__(message, * args)
+
+class Triangulo:
+
+    __lados: list[float]
+    semiPerimetro: float
+
+    def __init__(self, a: float, b: float, c: float) -> None:
+        self.__validarTraingulo([a, b, c])
+
+    def __init__(self, lados: list[float]) -> None:
+        self.__validarTraingulo(lados)
+
+    def __getSemiperimetro(self, lados: list) -> float:
+        return sum(lados)/2
+
+    def __validarTraingulo(self, lados: list) -> None:
+        for indice, lado in enumerate(lados):
+            if lado <= 0:
+                raise Excepcion(
+                    f'El argumento {indice+1} ({lado}) debe ser un numero real positivo.')
+        semi = self.__getSemiperimetro(lados)
+        for lado in lados:
+            if semi <= lado:
+                raise Excepcion("El triangulo no existe")
+        self.semiPerimetro = semi
+        self.__lados = lados
+
+    def heron(self, decimales=3) -> float:
+        parcial = 1
+        for lado in self.__lados:
+            parcial *= self.semiPerimetro - lado
+        return round(math.sqrt(self.semiPerimetro * parcial), decimales)
+    
+    def getA(self) -> float:
+        return self.__lados[0]
+    
+    def getB(self) -> float:
+        return self.__lados[1]
+    
+    def getC(self) -> float:
+        return self.__lados[2]
+    
+    def getLados(self) -> list[float]:
+        return self.__lados
+    
+    def setA(self, a: float) -> None:
+        self.__validarTraingulo([a, self.__lados[1], self.__lados[2]])
+            
+    def setB(self, b: float) -> None:
+        self.__validarTraingulo([self.__lados[0], b, self.__lados[2]])
+
+    def setC(self, c: float) -> None:
+        self.__validarTraingulo([self.__lados[0], self.__lados[1], c])
+
+
+lados = [5, 5, 5]
+triangulo = Triangulo(lados)
+area = triangulo.heron(2)
+print(f'El area del triangulo es: {area}')
+triangulo.setB(7)
+area = triangulo.heron(2)
+print(f'El area del triangulo es: {area}')
+print(triangulo.getLados())
---
+++ raices_polonomio_grado_2.py
@@ -0,0 +1,52 @@
+import math
+
+
+class Excepcion(ValueError):
+    def __init__(self, message, * args):
+        super(Excepcion, self).__init__(message, * args)
+
+
+class PolinomioGrado2:
+
+    __a: int
+    __b: int
+    __c: int
+
+    def __init__(self, a: float, b: float, c: float) -> None:
+        self.__asignarCoeficientes([a, b, c])
+
+    def __asignarCoeficientes(self, coeficientes: list[float]) -> None:
+        if coeficientes[0] == 0:
+            raise Excepcion(
+                f'El cofeiciente "a" debe ser diferente de 0.')
+        self.__a = coeficientes[0]
+        self.__b = coeficientes[1]
+        self.__c = coeficientes[2]
+
+    def calcularDiscriminate(self) -> float:
+        return math.pow(self.__b, 2)-4*self.__a*self.__c
+
+    def calcularRaices(self, decimales: int = 3) -> list[float]:
+        discriminate = self.calcularDiscriminate()
+        complejo = False
+        if (discriminate < 0):
+            raiz = math.sqrt(abs(discriminate))
+            complejo = True
+        else:
+            raiz = math.sqrt(discriminate)
+        t1 = -self.__b/(2*self.__a)
+        t2 = raiz/(2*self.__a)
+        if complejo:
+            x1 = (complex(round(t1, decimales), round(t2, decimales)))
+            x2 = (complex(round(t1, decimales), -round(t2, decimales)))
+        else:
+            x1 = (round(t1 + t2, decimales))
+            x2 = (round(t1 - t2, decimales))
+        return [x1, x2]
+
+
+a = float(input("coeficiente A >> "))
+b = float(input("coeficiente B >> "))
+c = float(input("coeficiente C >> "))
+funcion = PolinomioGrado2(a, b, c)
+print(funcion.calcularRaices())
