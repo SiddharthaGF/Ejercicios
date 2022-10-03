ade9c66d45264e43583682c21dc401a0fc940496
Pablo Santiago Guerra <61328784+SiddharthaGF@users.noreply.github.com>
2022-10-03 09:08:09 -0500

    Update raices_polonomio_grado_2.py

--- raices_polonomio_grado_2.py
+++ raices_polonomio_grado_2.py
@@ -1,10 +1,8 @@
 import math
 
-
 class Excepcion(ValueError):
     def __init__(self, message, * args):
         super(Excepcion, self).__init__(message, * args)
-
 
 class PolinomioGrado2:
 
@@ -43,10 +41,3 @@ class PolinomioGrado2:
             x1 = (round(t1 + t2, decimales))
             x2 = (round(t1 - t2, decimales))
         return [x1, x2]
-
-
-a = float(input("coeficiente A >> "))
-b = float(input("coeficiente B >> "))
-c = float(input("coeficiente C >> "))
-funcion = PolinomioGrado2(a, b, c)
-print(funcion.calcularRaices())
